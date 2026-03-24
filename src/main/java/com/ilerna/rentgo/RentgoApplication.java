9c9d8b0c0c31c989ad032564f462ced3e45a790e
pompito088-stack <pompito088@gmail.com>
2026-03-24 16:41:04 +0100

    En este commit he creado la base inicial del proyecto Spring Boot.

---
+++ src/main/java/com/ilerna/rentgo/RentgoApplication.java
@@ -0,0 +1,13 @@
+package com.ilerna.rentgo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RentgoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RentgoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ilerna/rentgo/RentgoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ilerna.rentgo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RentgoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
